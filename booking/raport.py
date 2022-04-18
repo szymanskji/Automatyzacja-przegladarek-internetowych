65f25934bba3a75b6327f7cdc59e778ace5294be
ben <bobusbogdan@gmail.com>
2022-04-19 00:20:10 +0200

    bart naprawilem

--- booking/raport.py
+++ booking/raport.py
@@ -12,15 +12,20 @@ class BookingRaport:
 
     def pobierz_hotele(self):
         wyniki = []
+        licznik = 1
         for okienko in self.okienka:
             nazwa = okienko.find_element(By.CSS_SELECTOR, 'div[data-testid="title"]').get_attribute('innerHTML')
             nazwa = nazwa.replace("&amp;", "&")
-            ocena ="x"
-            #ocena = okienko.find_element(By.CSS_SELECTOR, 'div.b5cd09854e.d10a6220b4').get_attribute('innerHTML')
+            temp = str(licznik)
+            try:
+                ocena = okienko.find_element(By.XPATH, "(//div[@class='b5cd09854e d10a6220b4'])[" + temp + "]").text
+            except:
+                break
             cena = okienko.find_element(By.CSS_SELECTOR, 'span.fcab3ed991.bd73d13072').get_attribute('innerHTML')
             cena = cena.replace(" ", "").replace("&nbsp;", " ")
             odleglosc = okienko.find_element(By.CSS_SELECTOR, 'span[data-testid="distance"]').get_attribute('innerHTML')
             wyniki.append(
                 [nazwa, cena, ocena, odleglosc]
             )
+            licznik = licznik +1
         return wyniki
